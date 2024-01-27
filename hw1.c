fdb66be28ba624f526fa2278371c8d5b3f2de975
Stephen Tinubu <60358867+Pro-tonn@users.noreply.github.com>
2024-01-26 23:50:29 -0600

    rework

--- hw1.c
+++ hw1.c
@@ -4,51 +4,90 @@
 #include <assert.h>
 
 
+
+
 static void methods()
 {
-    // TODO printf all global and weak functions in lib.o
-    //      one per line, in alphabetic order
-    //      your code should be something like:
-    //        printf("some_function\n");
-    //        printf("some_other_function\n");
-    //      you should have exactly one \n in each printf
-
-    assert(0); // Not implemented, when you have, remove this line.
+    printf("error_handler\n");
+    printf("is_invalid\n");
+    printf("signature\n");
+    printf("usage\n");
 }
-
 
 static void vars()
 {
-    // TODO printf all global and static variables in lib.o
-    //      one per line, in alphabetic order
-    //      your code should be something like:
-    //        printf("some_variable\n");
-    //        printf("some_other_variable\n");
-    //      you should have exactly one \n in each printf
-
-    assert(0); // Not implemented, when you have, remove this line.
+    printf("ERRORS\n");
+    printf("FULL\n");
+    printf("FULL_ERRORS\n");
+    printf("MODE\n");
+    printf("RESTRICTED\n");
+    printf("STATUSCODE\n");
 }
 
-// TODO define functions and variables you found in lib.o
+extern void usage();
+extern void vars();
+extern void methods();
+extern int signature(char* input, int len);
+extern int is_invalid(const char* input, size_t len);
+extern int* MODE;
+extern int STATUSCODE;
+extern int FULL;
+extern int RESTRICTED;
+
+void reverse_string(char* string) {
+    char* startPtr = string;
+    char* endPtr = string + strlen(string) - 1;
+
+    while (startPtr < endPtr) {
+        char temp = startPtr;
+        startPtr =endPtr;
+        endPtr = temp;
+
+        startPtr++;
+        endPtr--;
+    }
+}
 
 
-int main(int argc, char * argv[])
+int main(int argc, char argv[])
 {
     if (argc != 2) {
         fprintf(stderr, "Program needs at least one argument\n");
         return 1;
     }
 
-    char * arg = argv[1];
+    char arg = argv[1];
     if (!strcmp(arg, "methods")) {
         methods();
-    } else if (!strcmp(arg, "vars")) {
+    }
+    else if (!strcmp(arg, "vars")) {
         vars();
-    } else if (!strcmp(arg, "help")) {
-        assert(0); // NOT IMPLEMENTED remove this line once implemented
-    } else {
-        const int result = -1;
-        assert(0); // NOT IMPLEMENTED remove this line once implemented
+    }
+    else if (!strcmp(arg, "help")) {
+        usage();
+    }
+    else {
+        if (is_invalid(arg, strlen(arg)) == 0)
+        {
+            MODE = &RESTRICTED;
+        }
+        else
+        {
+            MODE = &FULL;
+        }
+        int sig = signature(arg, strlen(arg));
+
+        if (STATUSCODE == 0)
+        {
+            printf("%d", sig);
+        }
+        else
+        {
+            char reversed[strlen(arg) + 1];
+            strcpy(reversed, arg);
+            reverse_string(reversed);
+            printf("%s", reversed);
+        }
     }
 
     return 0;
--- makefile
+++ makefile
@@ -1,13 +1,10 @@
 all: hw1
 
-
 hw1: hw1.o
-	gcc -g -o hw1 hw1.o
-
+	gcc -g -o hw1 hw1.o lib.o
 
 hw1.o: hw1.c
 	gcc -g -c hw1.c
-
 
 clean:
 	rm -f hw1.o hw1
